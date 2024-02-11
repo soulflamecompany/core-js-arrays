b89e2cbeb21840ab2b169db9915e6475f03b9b42
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2024-02-12 00:34:57 +0300

    feat: + 48 / - 1

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -570,8 +570,14 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n > 0) {
+    return arr.slice(arr.length - n).concat(arr.slice(0, arr.length - n));
+  }
+  if (n < 0) {
+    return arr.slice(-n).concat(arr.slice(0, -n));
+  }
+  return arr;
 }
 
 /**
@@ -587,8 +593,21 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const digitValues = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  return arr.sort((a, b) => digitValues[a] - digitValues[b]);
 }
 
 /**
